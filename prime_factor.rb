9b3a8e807739e078e7da347c4958a21b6c3781d7
Dan Crews <dancrews@me.com>
2014-03-17 12:44:08 -0400

    Add tail recursive method

--- prime_factor.rb
+++ prime_factor.rb
@@ -1,10 +1,27 @@
-input test_number
 
-call factorize method
 
-calculate upper_bound prime factor of test_number
+test_number = 600851475143
 
-test for prime factors below upper_bound 
-    start with upper_bound and work down
+# def factorize(test_number)
+#   return [] if test_number == 1
+#   divisor = 2
+#   factor_array = []
+#   while test_number > 1
+#     while (test_number % divisor) == 0
+#       factor_array << divisor
+#       test_number /= divisor
+#     end
+#     divisor += 1
+#   end
+#   return factor_array
+# end
 
-return a factor_array containing factors of test_number  
+# puts factorize(test_number)
+
+def factorize(test_number)
+  return [] if test_number == 1
+  prime_factor = (2..test_number).find {|divisor| (test_number % divisor) == 0}
+  [prime_factor] + factorize(test_number / prime_factor)
+end
+
+puts factorize(test_number)
