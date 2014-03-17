7ddeb731a787cb13600b360eac26ae3f7e83a8b0
Dan Crews <dancrews@me.com>
2014-03-17 10:21:29 -0400

    Initial commit

---
+++ prime_factor.rb
@@ -0,0 +1,10 @@
+input test_number
+
+call factorize method
+
+calculate upper_bound prime factor of test_number
+
+test for prime factors below upper_bound 
+    start with upper_bound and work down
+
+return a factor_array containing factors of test_number  
