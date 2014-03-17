6fd57080dddd33f45b67bcd6cc8a0e00b00b9cae
Dan Crews <dancrews@me.com>
2014-03-17 14:09:38 -0400

    3 ways to prime factorize

--- prime_factor.rb
+++ prime_factor.rb
@@ -2,26 +2,37 @@
 
 test_number = 600851475143
 
-# def factorize(test_number)
-#   return [] if test_number == 1
-#   divisor = 2
-#   factor_array = []
-#   while test_number > 1
-#     while (test_number % divisor) == 0
-#       factor_array << divisor
-#       test_number /= divisor
-#     end
-#     divisor += 1
-#   end
-#   return factor_array
-# end
-
-# puts factorize(test_number)
+# This is the first version I came up with
 
 def factorize(test_number)
   return [] if test_number == 1
-  prime_factor = (2..test_number).find {|divisor| (test_number % divisor) == 0}
+  divisor = 2
+  factor_array = []
+  while test_number > 1
+    while (test_number % divisor) == 0
+      factor_array << divisor
+      test_number /= divisor
+    end
+    divisor += 1
+  end
+  return factor_array.max
+end
+puts factorize(test_number)
+
+#-----------------------------------------------------------------------------
+
+# Did some research and found a tail recursive algorithm
+
+def factorize(test_number)
+  return [] if test_number == 1
+  prime_factor = (2..test_number).find {|divisor| test_number % divisor == 0}
   [prime_factor] + factorize(test_number / prime_factor)
 end
+puts factorize(test_number).max
 
-puts factorize(test_number)
+#-----------------------------------------------------------------------------
+
+# Ruby has a prime library that can be called to accomplish
+
+require 'prime'
+puts test_number.prime_division.max[0]
